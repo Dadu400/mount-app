efadf8a2ec3664d335ff05aa613cdb4ac46a9b94
Dadu400 <daduka.xuxunashvili@gmail.com>
2025-02-03 16:20:33 +0400

    Add banner images and change responsive design for better ui

--- components/banners/DownloadBanner.tsx
+++ components/banners/DownloadBanner.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image';
-
 import { useTranslations, useLocale } from "next-intl";
 
 import Apple from '../../icons/appstore.svg';
 import QR from '../../icons/qr.png';
-
+import AppBanner from "../../icons/appbaner.png";
 
 function DownloadBanner() {
     const t = useTranslations("download");
@@ -15,26 +14,44 @@ function DownloadBanner() {
     });
 
     return (
-        <section className="w-[90%] md:w-[85%] lg:w-[80%] xl:w-[75%] 2xl:w-[65%] mx-auto py-10 sm:py-20">
-            <div className="bg-white/10 rounded-3xl p-12 backdrop-blur-sm text-center transform hover:scale-105 transition-transform duration-300 flex flex-col gap-y-4">
-                <h2 className={`text-3xl leading-tight text-white ${locale === "ka" ? "font-tbcMedium" : "font-avenirHeavy font-extrabold"}`}
-                        dangerouslySetInnerHTML={{ __html: title }}></h2>
-                <p className="text-xl text-white/80 my-4">
+        <section className="w-[85%] md:w-[85%] lg:w-[80%] xl:w-[70%] 2xl:w-[65%] mx-auto py-10 sm:py-16">
+            <div className="bg-white/10 rounded-3xl p-10 text-pretty text-center backdrop-blur-sm transform hover:scale-105 transition-transform duration-300 flex flex-col gap-y-4">
+                <h2
+                    className={`leading-tight text-white ${locale === "ka" ? "font-tbcBold text-4xl" : "font-avenirHeavy font-extrabold text-4xl"}`}
+                    dangerouslySetInnerHTML={{ __html: title }}
+                ></h2>
+                <div className="flex justify-center">
+                    <Image
+                        src={AppBanner}
+                        alt="Mountapp Banner"
+                        quality={100}
+                        width={500}
+                        height={350}
+                        layout="intrinsic"
+                        className="rounded-xl max-w-full"
+                        priority
+                    />
+                </div>
+                <p className={`text-white/80 my-4 ${locale === "ka" ? "font-tbcRegular text-lg" : "font-avenir text-xl"}`}>
                     {t("description")}
                 </p>
-                <div className="flex flex-col sm:flex-row items-center flex-wrap justify-around sm:items-center gap-4">
+                <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
                     <a
                         href="https://apps.apple.com/ge/app/mountapp/id6466344143"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="w-28 sm:w-40 hover:opacity-90"
                     >
-                        <Image
-                            src={Apple}
-                            alt="Apple Store"
-                            quality={100} />
+                        <Image src={Apple} alt="Apple Store" quality={100} width={160} height={50} />
                     </a>
-                    <Image src={QR} alt='qr-code' className='w-28 h-28 rounded-xl border-black border-2' quality={100} />
+                    <Image
+                        src={QR}
+                        alt="QR Code"
+                        quality={100}
+                        width={112}
+                        height={112}
+                        className="rounded-xl border-black border-2"
+                    />
                 </div>
             </div>
         </section>
--- components/banners/LocationsBanner.tsx
+++ components/banners/LocationsBanner.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
-
 import { useTranslations, useLocale } from "next-intl";
 
 import BakurianiIcon from "../../icons/bakuriani.png";
 import GudauriIcon from "../../icons/gudauri.png";
 import MestiaIcon from "../../icons/mestia.png";
 import GoderdziIcon from "../../icons/goderdzi.png";
-import BannerImage from "../../icons/screen.png";
+import BannerRent from "../../icons/bannerRent.png";
 
 function LocationBanner() {
   const t = useTranslations("banner");
@@ -24,39 +23,43 @@ function LocationBanner() {
   ];
 
   return (
-    <section className="w-[96%] sm:w-[90%] md:w-[85%] lg:w-[80%] xl:w-[75%] 2xl:w-[65%] mx-auto my-12 sm:my-24">
-      <div className="flex flex-col items-center sm:flex-row justify-between gap-20">
-        <div className="w-1/3">
-          <div className="relative">
-            <Image
-              src={BannerImage}
-              alt="Mountapp Banner"
-              quality={100}
-              width={500}
-              height={100}
-              className="rounded-xl"
-            />
-          </div>
+    <section className="w-full sm:w-[90%] md:w-[85%] lg:w-[80%] xl:w-[75%] 2xl:w-[65%] sm:mx-auto my-12 sm:my-24">
+      <div className="flex flex-col items-center text-center gap-y-10 lg:flex-row lg:text-start sm:gap-10 lg:gap-y-14">
+        <div className="w-full lg:w-1/2 flex justify-center lg:justify-start">
+          <Image
+            src={BannerRent}
+            alt="Mountapp Banner"
+            quality={100}
+            width={500}
+            height={350}
+            className="rounded-xl max-w-[80%] sm:max-w-[90%] lg:max-w-[98%] xl:max-w-none mx-auto lg:mx-0"
+            priority
+          />
         </div>
-        <div className="w-2/3 space-y-6 order-1 md:order-2 flex flex-col">
-          <h2 className={`text-3xl md:text-5xl ${locale === "ka" ? "font-tbcMedium" : "font-avenirHeavy font-extrabold leading-tight"}`}
-            dangerouslySetInnerHTML={{ __html: title }}>
-          </h2>
-          <p className={`text-xl text-white/80 font-avenir ${locale === "ka" ? "font-tbcRegular" : "font-avenir"} `}>
+
+        <div className="w-full lg:w-1/2 flex flex-col items-center lg:items-end gap-y-6 sm:gap-y-8">
+          <h2
+            className={`text-white leading-tight ${locale === "ka" ? "font-tbcBold text-3xl sm:text-4xl xl:text-5xl w-[90%] mx-auto lg:mx-0" : "font-avenirHeavy text-4xl sm:text-5xl xl:text-6xl font-extrabold w-[90%] mx-auto lg:mx-0"}`}
+            dangerouslySetInnerHTML={{ __html: title }}
+          ></h2>
+
+          <p className={`text-lg text-gray-300 ${locale === "ka" ? "font-tbcRegular" : "font-avenirHeavy"} w-[90%] mx-auto lg:mx-0`}>
             {t("description")}
           </p>
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-y-8 gap-x-2">
+
+          <div className="grid grid-cols-2 sm:grid-cols-4 gap-y-6 lg:gap-y-8 gap-x-4 justify-center items-center lg:justify-self-start">
             {skiDestinations.map((destination, index) => (
-              <div key={index} className="relative w-full">
+              <div key={index} className="relative w-full flex items-center lg:self-start">
                 <Image
                   src={destination.src}
                   alt={destination.alt}
                   quality={100}
-                  className="w-28 h-28"
+                  className="w-[70px] sm:w-[80px] md:w-[90px] lg:w-[80px] xl:w-[90px] h-auto object-contain"
                 />
               </div>
             ))}
           </div>
+
         </div>
       </div>
     </section>
